1a434c8026a9ab7001176d77657522e4a696d1a2
Łukasz Klein <lctaking@gmail.com>
2017-12-29 13:22:31 +0100

    v1

---
+++ książkakucharska/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace książkakucharska
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.warzywa = new System.Windows.Forms.CheckedListBox();
+            this.mięso = new System.Windows.Forms.CheckedListBox();
+            this.nabiał = new System.Windows.Forms.CheckedListBox();
+            this.zboża = new System.Windows.Forms.CheckedListBox();
+            this.owoce = new System.Windows.Forms.CheckedListBox();
+            this.inne = new System.Windows.Forms.CheckedListBox();
+            this.dzialanie = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // warzywa
+            // 
+            this.warzywa.FormattingEnabled = true;
+            this.warzywa.Items.AddRange(new object[] {
+            "pomidory",
+            "kapusta pekińska",
+            "ogórki kiszone",
+            "kukurydza",
+            "kiszona kapusta",
+            "ziemniaki",
+            "cebula",
+            "marchewka",
+            "ogórek świeży",
+            "sałata"});
+            this.warzywa.Location = new System.Drawing.Point(12, 12);
+            this.warzywa.Name = "warzywa";
+            this.warzywa.Size = new System.Drawing.Size(120, 169);
+            this.warzywa.TabIndex = 1;
+            // 
+            // mięso
+            // 
+            this.mięso.FormattingEnabled = true;
+            this.mięso.Items.AddRange(new object[] {
+            "wieprzowina",
+            "kurczak",
+            "kiełbasa",
+            "ryba"});
+            this.mięso.Location = new System.Drawing.Point(138, 12);
+            this.mięso.Name = "mięso";
+            this.mięso.Size = new System.Drawing.Size(120, 169);
+            this.mięso.TabIndex = 2;
+            // 
+            // nabiał
+            // 
+            this.nabiał.FormattingEnabled = true;
+            this.nabiał.Items.AddRange(new object[] {
+            "jajka",
+            "ser",
+            "śmietana"});
+            this.nabiał.Location = new System.Drawing.Point(264, 12);
+            this.nabiał.Name = "nabiał";
+            this.nabiał.Size = new System.Drawing.Size(120, 169);
+            this.nabiał.TabIndex = 3;
+            // 
+            // zboża
+            // 
+            this.zboża.FormattingEnabled = true;
+            this.zboża.Items.AddRange(new object[] {
+            "makaron",
+            "mąka",
+            "placek pszenny"});
+            this.zboża.Location = new System.Drawing.Point(390, 12);
+            this.zboża.Name = "zboża";
+            this.zboża.Size = new System.Drawing.Size(120, 169);
+            this.zboża.TabIndex = 4;
+            // 
+            // owoce
+            // 
+            this.owoce.FormattingEnabled = true;
+            this.owoce.Location = new System.Drawing.Point(516, 12);
+            this.owoce.Name = "owoce";
+            this.owoce.Size = new System.Drawing.Size(120, 169);
+            this.owoce.TabIndex = 5;
+            // 
+            // inne
+            // 
+            this.inne.FormattingEnabled = true;
+            this.inne.Items.AddRange(new object[] {
+            "przecier pomidorowy",
+            "ketchup",
+            "majonez",
+            "przyprawa do mięsa",
+            "koncentrat pomidorowy",
+            "sos pomidorowy",
+            "cukier"});
+            this.inne.Location = new System.Drawing.Point(642, 12);
+            this.inne.Name = "inne";
+            this.inne.Size = new System.Drawing.Size(135, 169);
+            this.inne.TabIndex = 6;
+            // 
+            // dzialanie
+            // 
+            this.dzialanie.Location = new System.Drawing.Point(390, 237);
+            this.dzialanie.Name = "dzialanie";
+            this.dzialanie.Size = new System.Drawing.Size(75, 23);
+            this.dzialanie.TabIndex = 7;
+            this.dzialanie.Text = "dzialaj";
+            this.dzialanie.UseVisualStyleBackColor = true;
+            this.dzialanie.Click += new System.EventHandler(this.dzialanie_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(600, 218);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(300, 175);
+            this.listView1.TabIndex = 8;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(912, 405);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.dzialanie);
+            this.Controls.Add(this.inne);
+            this.Controls.Add(this.owoce);
+            this.Controls.Add(this.zboża);
+            this.Controls.Add(this.nabiał);
+            this.Controls.Add(this.mięso);
+            this.Controls.Add(this.warzywa);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox warzywa;
+        private System.Windows.Forms.CheckedListBox mięso;
+        private System.Windows.Forms.CheckedListBox nabiał;
+        private System.Windows.Forms.CheckedListBox zboża;
+        private System.Windows.Forms.CheckedListBox owoce;
+        private System.Windows.Forms.CheckedListBox inne;
+        private System.Windows.Forms.Button dzialanie;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ książkakucharska/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace książkakucharska
+{
+    public partial class Form1 : Form
+    {
+        public void ShowDish(Stack Ing, string Dish)//funkcja przyjmująca składniki i nazwę dania do wyświetlenia w listview
+        {
+            bool IsFirst = true;
+            string[] arr = new string[2];
+            ListViewItem itm;
+            int Length = Ing.Count;
+            for (int i = 0; i < Length; i++)
+            {
+                if (IsFirst)
+                {
+                    arr[0] = Dish;
+                    arr[1] = (string)Ing.Pop();
+                    IsFirst = false;
+                    itm = new ListViewItem(arr);
+                    listView1.Items.Add(itm);
+                }
+                else
+                {
+                    arr[0] = "";
+                    arr[1] = (string)Ing.Pop();
+                    itm = new ListViewItem(arr);
+                    listView1.Items.Add(itm);
+                }
+            }
+            IsFirst = true;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            listView1.View = View.Details;
+            listView1.GridLines = true;
+            listView1.FullRowSelect = true;
+            listView1.Columns.Add("Danie", 100);
+            listView1.Columns.Add("Składniki", 200);
+        }
+
+        private void dzialanie_Click(object sender, EventArgs e)
+        {
+            if (nabiał.GetItemCheckState(0).ToString() == "Checked")
+            {
+                Stack Ingredients = new Stack();
+                Ingredients.Push("jajka");
+                ShowDish(Ingredients, "jajecznica");
+            }
+
+            if (inne.GetItemCheckState(0).ToString() == "Checked" && warzywa.GetItemCheckState(0).ToString() == "Checked" && mięso.GetItemCheckState(0).ToString() == "Checked" & zboża.GetItemCheckState(0).ToString() == "Checked")
+            {
+                Stack Ingredients = new Stack();
+                Ingredients.Push("pomidory");
+                Ingredients.Push("makaron");
+                Ingredients.Push("wieprzowina");
+                Ingredients.Push("przecier pomidorowy");
+                ShowDish(Ingredients, "spaghetti");
+            }
+
+            if (warzywa.GetItemCheckState(1).ToString() == "Checked" && warzywa.GetItemCheckState(2).ToString() == "Checked" && warzywa.GetItemCheckState(3).ToString() == "Checked" && mięso.GetItemCheckState(1).ToString() == "Checked" && inne.GetItemCheckState(1).ToString() == "Checked" && inne.GetItemCheckState(2).ToString() == "Checked" && inne.GetItemCheckState(3).ToString() == "Checked")
+            {
+                Stack Ingredients = new Stack();
+                Ingredients.Push("kukurydza");
+                Ingredients.Push("kapusta pekińska");
+                Ingredients.Push("ogórki kiszone");
+                Ingredients.Push("kurczak");
+                Ingredients.Push("ketchup");
+                Ingredients.Push("majonez");
+                Ingredients.Push("przyprawa do mięsa");
+                ShowDish(Ingredients, "gyros");
+            }
+
+            if (warzywa.GetItemCheckState(4).ToString() == "Checked" && mięso.GetItemCheckState(0).ToString() == "Checked" && mięso.GetItemCheckState(2).ToString() == "Checked" && inne.GetItemCheckState(4).ToString() == "Checked")
+            {
+                Stack Ingredients = new Stack();
+                Ingredients.Push("kapusta kiszona");
+                Ingredients.Push("wieprzowina");
+                Ingredients.Push("kiełbasa");
+                Ingredients.Push("koncentrat pomidorowy");
+                ShowDish(Ingredients, "bigos");
+            }
+
+            if (warzywa.GetItemCheckState(5).ToString() == "Checked" && mięso.GetItemCheckState(0).ToString() == "Checked" && nabiał.GetItemCheckState(0).ToString() == "Checked" && zboża.GetItemCheckState(1).ToString() == "Checked")
+            {
+                Stack Ingredients = new Stack();
+                Ingredients.Push("ziemniaki");
+                Ingredients.Push("wieprzowina");
+                Ingredients.Push("jajka");
+                Ingredients.Push("mąka");
+                ShowDish(Ingredients, "kotlety schabowe");
+            }
+
+            if (warzywa.GetItemCheckState(6).ToString() == "Checked" && warzywa.GetItemCheckState(7).ToString() == "Checked" && mięso.GetItemCheckState(3).ToString() == "Checked" && inne.GetItemCheckState(0).ToString() == "Checked")
+            {
+                Stack Ingredients = new Stack();
+                Ingredients.Push("cebula");
+                Ingredients.Push("marchewka");
+                Ingredients.Push("ryba");
+                Ingredients.Push("przecier pomidorowy");
+                ShowDish(Ingredients, "ryba po grecku");
+            }
+
+            if (warzywa.GetItemCheckState(0).ToString() == "Checked" && warzywa.GetItemCheckState(8).ToString() == "Checked" && warzywa.GetItemCheckState(9).ToString() == "Checked" && mięso.GetItemCheckState(1).ToString() == "Checked" && zboża.GetItemCheckState(2).ToString() == "Checked")
+            {
+                Stack Ingredients = new Stack();
+                Ingredients.Push("pomidory");
+                Ingredients.Push("ogórek świeży");
+                Ingredients.Push("kurczak");
+                Ingredients.Push("sałata");
+                Ingredients.Push("placek pszenny");
+                ShowDish(Ingredients, "tortilla");
+            }
+
+            if (nabiał.GetItemCheckState(2).ToString() == "Checked" && nabiał.GetItemCheckState(1).ToString() == "Checked" && zboża.GetItemCheckState(0).ToString() == "Checked" && mięso.GetItemCheckState(0).ToString() == "Checked" && inne.GetItemCheckState(6).ToString() == "Checked")
+            {
+                Stack Ingredients = new Stack();
+                Ingredients.Push("makaron");
+                Ingredients.Push("sos pomidorowy");
+                Ingredients.Push("wieprzowina");
+                Ingredients.Push("śmietana");
+                Ingredients.Push("ser");
+                ShowDish(Ingredients, "lasagne");
+            }
+
+            if (nabiał.GetItemCheckState(2).ToString() == "Checked" && nabiał.GetItemCheckState(1).ToString() == "Checked" && zboża.GetItemCheckState(0).ToString() == "Checked" && mięso.GetItemCheckState(0).ToString() == "Checked" && inne.GetItemCheckState(6).ToString() == "Checked")
+            {
+                Stack Ingredients = new Stack();
+                Ingredients.Push("makaron");
+                Ingredients.Push("sos pomidorowy");
+                Ingredients.Push("wieprzowina");
+                Ingredients.Push("śmietana");
+                Ingredients.Push("ser");
+                ShowDish(Ingredients, "lasagne");
+            }
+
+            if (nabiał.GetItemCheckState(3).ToString() == "Checked" && zboża.GetItemCheckState(1).ToString() == "Checked" && nabiał.GetItemCheckState(0).ToString() == "Checked" && inne.GetItemCheckState(6).ToString() == "Checked")
+            {
+                Stack Ingredients = new Stack();
+                Ingredients.Push("mąka");
+                Ingredients.Push("mleko");
+                Ingredients.Push("jajka");
+                Ingredients.Push("cukier");
+                ShowDish(Ingredients, "naleśniki");
+            }
+        }
+    }
+}
